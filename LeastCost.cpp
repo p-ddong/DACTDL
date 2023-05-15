b72bca8b48500ac78ca33775f035dc4e50b0cbbb
Hankrezu <120353312+Hankrezu@users.noreply.github.com>
2023-05-16 06:39:42 +0700

    Update LeastCost.cpp

--- LeastCost.cpp
+++ LeastCost.cpp
@@ -15,6 +15,9 @@ struct MT // ma trận bài toán vận tải
     int b[MAX];// các điểm cầu
     int c[MAX][MAX];// cước phí
     int x[MAX][MAX]={0};// ma trận chuyển hàng
+    int value=0;// mức độ ưu tiên
+    int tongcung=0;// tổng số hàng cung
+    int tongcau=0;// tổng số hàng cầu
 };
 class LeastCost{
     struct MT *mtr1;
@@ -26,9 +29,6 @@ public:
 };
 //ThietLapMT sẽ làm nhiệm vụ nhập dữ liệu và cân bằng cung cầu
 void LeastCost::ThietLapMT(MT *mtr1){
-    int cung = 0;
-    int cau = 0;
-    int sosanh;
     cout<<" Số trạm cung ứng : ";
     cin >> mtr1->m;
     cout<<" Số trạm nhận hàng : ";
@@ -47,23 +47,25 @@ void LeastCost::ThietLapMT(MT *mtr1){
     for (int i = 0; i <  mtr1->m; i++) {
         cout << "Trạm cung " << i+1 << ": ";
         cin >> mtr1->a[i];
-        cung = cung + mtr1->a[i];
+       mtr1->tongcung = mtr1->tongcung + mtr1->a[i];
     };
     
     cout<<"Nhập số hàng cần nhận :"<<endl;
     for (int i = 0; i <  mtr1->n; i++) {
         cout << "Trạm cầu " << i+1 << ": ";
         cin >> mtr1->b[i];
-        cau = cau + mtr1->b[i];
+        mtr1->tongcau = mtr1->tongcau + mtr1->b[i];
     }; 
-    
-    if (cung > cau){
-        sosanh = 1;
-    } else if (cung < cau){
-        sosanh = 2;
+    // cân bằng cung cầu
+    if (mtr1->tongcung > mtr1->tongcau){
+        mtr1->value = 1;
+    } else if (mtr1->tongcung < mtr1->tongcau){
+        mtr1->value = 2;
+    }else{
+        return;
     }
-    int count = abs(cung-cau);
-    switch(sosanh){
+    int count = abs(mtr1->tongcung-mtr1->tongcau);
+    switch(mtr1->value){
         case 1:
             mtr1->n = mtr1->n + 1;
             // sao chép ma trận cũ sang ma trận mới c2 và thêm cột mới
@@ -108,24 +110,126 @@ void LeastCost::ThietLapMT(MT *mtr1){
 //ChiaHang sẽ làm nhiệm vụ chia hàng từ cung tới cầu 
 void LeastCost::Chiahang(MT *mtr1)
 {
- 
+struct MT *mtr2 = mtr1;
+int smallest = 1000; // khởi tạo giá trị nhỏ nhất cho biến smallest
+while(mtr2->tongcung != 0 || mtr2->tongcau !=0){
+    int m,n;
+    for(int i=0; i<mtr2->m; i++) {
+        for(int j=0; j<mtr2->n; j++) {
+            if(mtr2->c[i][j] != 0 && mtr2->c[i][j] < smallest) { // nếu phần tử khác 0 và nhỏ hơn smallest
+                smallest = mtr2->c[i][j];
+                m=i-1;
+                n=j-1;
+            }
+        }
+    }
+if(mtr2->a[m] == 0 || mtr2->b[n] == 0 ){
+    mtr2->c[m][n]=0;
+}else if (mtr2->a[m]>mtr2->b[n]){
+    mtr2->x[m][n]=mtr2->b[n];
+    mtr2->tongcung=mtr2->tongcung-mtr2->a[m]; 
+    mtr2->tongcau=mtr2->tongcau-mtr2->b[n];
+    mtr2->a[m]=mtr2->a[m]-mtr2->b[n];
+    mtr2->b[n]=0;
+    mtr2->c[m][n]=0;
+    return;
+}else if(mtr2->a[m]<mtr2->b[n]){
+    mtr2->x[m][n]=mtr2->a[m];
+    mtr2->tongcung=mtr2->tongcung-mtr2->a[m]; 
+    mtr2->tongcau=mtr2->tongcau-mtr2->b[n];
+    mtr2->b[n]=mtr2->b[n]-mtr2->a[m];
+    mtr2->a[m]=0;
+    mtr2->c[m][n]=0;
+    return;
+}else if (mtr2->a[m]==mtr2->b[n]){
+    mtr2->x[m][n]=mtr2->a[m];
+    mtr2->tongcung=mtr2->tongcung-mtr2->a[m]; 
+    mtr2->tongcau=mtr2->tongcau-mtr2->b[n];
+    mtr2->a[m]=0;
+    mtr2->b[n]=0;
+    mtr2->c[m][n]=0;
+    return;
+}else {
+    return;
+};
+
+}
+if (mtr2->tongcung == 0 && mtr2->tongcau == 0){
+    return;
+}else if ( mtr2->tongcung == 0 && mtr2->tongcau != 0){
+for ( int i = 0; i < mtr2->n; i++)
+{
+  mtr2->x[mtr2->m-1][i]=mtr2->b[i];
+  mtr2->b[i]=0;  
+}
+return;
+}else if (mtr2->tongcung != 0 && mtr2->tongcau == 0){
+    for ( int i = 0; i < mtr2->m; i++)
+{
+  mtr2->x[i][mtr2->n-1]=mtr2->a[i];
+  mtr2->a[i]=0;  
+}
+return;
+}else{ 
+return;
+}
+for (int i = 0; i < mtr1->m; i++) {
+                for (int j = 0; j < mtr1->n; j++) {
+                    mtr1->x[i][j] = mtr2->x[i][j];
+                };
+};
 }
 
 void LeastCost::TongCuocPhi(MT mtr1)
 {
-
+    int tongcp = 0;
+    for (int i = 0; i < mtr1.m; i++) {
+        for (int j = 0; j < mtr1.n; j++) {
+            if (mtr1.x[i][j] > 0) { // nếu có hàng được vận chuyển từ cung i đến cầu j
+                tongcp += mtr1.x[i][j] * mtr1.c[i][j]; // tính tổng chi phí
+            }
+        }
+    }
+    cout << "Tổng chi phí là: " << tongcp << endl;
 }
-
 void LeastCost::Hienthi(MT mtr1)
 {
-
+    cout << "Ma tran van tai:\n";
+    cout << "cung/cau ";
+    for (int j = 0; j < mtr1.n; j++) {
+        cout << "b" << j+1 << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < mtr1.m; i++) {
+        cout << "a" << i+1 << "      ";
+        for (int j = 0; j < mtr1.n; j++) {
+            cout << mtr1.c[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "\nMa tran phan bo:\n";
+    cout << "cung/cau ";
+    for (int j = 0; j < mtr1.n; j++) {
+        cout << "b" << j+1 << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < mtr1.m; i++) {
+        cout << "a" << i+1 << "      ";
+        for (int j = 0; j < mtr1.n; j++) {
+            cout << mtr1.a[i*mtr1.n+j] << " ";
+        }
+        cout << endl;
+    }
 }
-
 int main() {
     MT mtr;
     LeastCost lc;
     lc.ThietLapMT(&mtr);
+    lc.Chiahang(&mtr);
+    lc.Hienthi(mtr);
+    lc.TongCuocPhi(mtr);
     return 0;
 }
+
 
 
